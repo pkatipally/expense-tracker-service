e955893e88a42792394dbc6b6c74a85b51ca8a68
Priya <priyakatipally@gmail.com>
2020-03-19 22:27:44 -0400

    first commit

---
+++ src/test/java/com/priya/expensetrackerservice/ExpenseTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priya.expensetrackerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
